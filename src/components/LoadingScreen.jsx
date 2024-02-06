fc391dc72b011570d3848d89026d5f4e3d7ce221
Sabbirhossain97 <sabbirhossainbd199@gmail.com>
2024-02-06 12:58:26 +0600

    nothing new

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -4,7 +4,7 @@ export default function LoadingScreen() {
   return (
     <div className="font-inter h-screen flex flex-col justify-center items-center bg-slate-800">
       <div className=" mx-auto">
-        <img src="/weather.svg" width="70px" height="70px" className="ml-10" />
+        <img src="/weather.svg" width="70px" height="70px" className="ml-10" alt="logo"/>
       </div>
     </div>
   );
